ed1b6db046679aa0b48bd6913d3e969013d4ad41
JosVuHuynh <vuhuynh9x@gmail.com>
2022-05-03 22:09:49 +0700

    verify merkle with amount equal zero

--- programs/distribute-token-solana/src/lib.rs
+++ programs/distribute-token-solana/src/lib.rs
@@ -39,9 +39,7 @@ pub mod distribute_token_solana {
             &claimer.key().to_bytes(),
             &amount.to_le_bytes(),
         ]);
-
-       ;
-
+        
         if !verify(proof, distributor.root, node.0) {
             return Err(Errors::InvalidMerkleProof.into());
         }
@@ -57,8 +55,7 @@ pub mod distribute_token_solana {
         let seeds  = [ 
             b"Distributor".as_ref(), 
             &distributor.distributor_key.to_bytes(), 
-            //&[ctx.accounts.distributor.bump],
-            &[_bump]
+            &[ctx.accounts.distributor.bump],
             ];
         token::transfer(
             CpiContext::new(
@@ -173,11 +170,11 @@ pub fn verify(proof: Vec<[u8; 32]>, root: [u8; 32], leaf: [u8; 32]) -> bool {
         if computed_hash <= proof_element {
             // Hash(current computed hash + current element of the proof)
             computed_hash =
-                anchor_lang::solana_program::keccak::hashv(&[&computed_hash, &proof_element]).to_bytes();
+                anchor_lang::solana_program::keccak::hashv(&[&computed_hash, &proof_element]).0;
         } else {
             // Hash(current element of the proof + current computed hash)
             computed_hash =
-                anchor_lang::solana_program::keccak::hashv(&[&proof_element, &computed_hash]).to_bytes();
+                anchor_lang::solana_program::keccak::hashv(&[&proof_element, &computed_hash]).0;
         }
     }
     // Check if the computed hash (root) is equal to the provided root
--- scripts/distribute.service.ts
+++ scripts/distribute.service.ts
@@ -1,22 +1,19 @@
 import { BN } from '@project-serum/anchor';
 import * as borsh from '@project-serum/borsh';
-import { ACCOUNT_SIZE, createAccount, createInitializeAccountInstruction, createMint, getAccount,getMinimumBalanceForRentExemptAccount,mintTo,TOKEN_PROGRAM_ID } from '@solana/spl-token';
+import { ACCOUNT_SIZE, createAccount, createInitializeAccountInstruction, createMint, createMintToCheckedInstruction, getAccount,getMinimumBalanceForRentExemptAccount,getMint,getOrCreateAssociatedTokenAccount,mintTo,mintToChecked,TOKEN_PROGRAM_ID } from '@solana/spl-token';
 import { TokenPocketWalletName } from '@solana/wallet-adapter-wallets';
 import { AccountMeta, Transaction, TransactionInstruction, PublicKey, Connection, Keypair, LAMPORTS_PER_SOL, sendAndConfirmTransaction, SystemProgram } from '@solana/web3.js';
-import { logger } from 'ethers';
-import BalanceTree from './balance-tree';
 import { BorshService } from './borsh.service';
 import { HashService } from './hash.service';
 import MerkleTree from 'merkletreejs';
-import { sha256 } from 'js-sha256';
 import keccak256 from 'keccak256'
+import { keccak_256 } from "js-sha3";
  
 
 
 //let connection = new Connection('https://api.devnet.solana.com', 'confirmed')
 let connection = new Connection('http://127.0.0.1:8899', 'confirmed')
 let programId = new PublicKey('GLfs2uXqmGoun5X4eTVxS7TcRPqw5dnsWvtEPzJzAq4v')
-
 
 
 export class CreateDistributor {
@@ -44,6 +41,14 @@ export class ClaimRequest {
     proof: Buffer[]
 }
 
+export class CreateAccountDistributor {
+    mint: PublicKey
+    payer: Keypair
+    tokenAccountSender: PublicKey
+    distributorAddress: PublicKey
+    bump: number
+}
+
 const DISTRIBUTE_PROGRAM_LAYOUTS = {
     CREATE_DISTRIBUTOR: <borsh.Layout<CreateDistributor>> borsh.struct([
         borsh.u8('bump'),
@@ -69,50 +74,21 @@ export async function getAirdrop(key: PublicKey) {
     console.log('using account',sig);
 }
 
-//export class DistributeService {
 export async function createDistributor(
-    bump: number,
     root: Buffer,
     totalClaimed: BN,
     distributeProgramId: PublicKey,
+    _mint: PublicKey,
+    _payer: Keypair,
+    _distributorAddress: PublicKey,
+    _bump: number
 ): Promise<void> {
-
-
-    let payer = Keypair.generate();
-    await getAirdrop(payer.publicKey)
-    let mintPubkey = await createMint(
-        connection, // conneciton
-        payer, // fee payer
-        payer.publicKey, // mint authority
-        payer.publicKey, // freeze authority (you can use `null` to disable it. when you disable it, you can't turn it on again)
-        8 // decimals
-      );
-    
-      const tokenAccount = Keypair.generate();
-      console.log(`token account sender ${tokenAccount.publicKey.toBase58()}`);
-      const [distributorAddress, _bump] = await findDistributorAddress(payer.publicKey, programId);
-
-      let tx = new Transaction().add(
-        // create token account
-        SystemProgram.createAccount({
-          fromPubkey: payer.publicKey,
-          newAccountPubkey: tokenAccount.publicKey,
-          space: ACCOUNT_SIZE,
-          lamports: await getMinimumBalanceForRentExemptAccount(connection),
-          programId: TOKEN_PROGRAM_ID,
-        }),
-        // init mint account
-        createInitializeAccountInstruction(
-          tokenAccount.publicKey, // token account
-          mintPubkey, // mint
-          //payer.publicKey // owner of token account
-          distributorAddress
-        )
-      );
-      console.log(`txhash: ${await connection.sendTransaction(tx, [payer, tokenAccount])}`);
+    const payer = _payer
+    const mintPubkey = _mint
+    const distributorAddress = _distributorAddress
     const transaction: Transaction = new Transaction();
     const request: CreateDistributorRequest = {
-        bump: bump,
+        bump: _bump,
         root: root,
         totalClaimed: totalClaimed
     }
@@ -128,6 +104,8 @@ export async function createDistributor(
 
     console.log("mintPubkey is", mintPubkey.toBase58())
     console.log("payer is", payer.publicKey.toBase58())
+
+
     const keys: AccountMeta[] = [
         <AccountMeta>{pubkey: payer.publicKey, isSigner: true, isWritable: true },
         <AccountMeta>{pubkey: distributorAddress, isSigner: false, isWritable: true },
@@ -150,14 +128,22 @@ export async function createDistributor(
 }
 
 export async function claim(
-    bump : number,
     index: BN,
     amount: BN,
     proof: Buffer[],
+    _mintPubkey: PublicKey,
+    _tokenAccountSender: PublicKey,
+    _distributeAddress: PublicKey
+
 ):Promise<void> {
-        
+    const tokenAccountSender = _tokenAccountSender
+    const distributorAddress = _distributeAddress
+    let mintPubkey = _mintPubkey
+
+    const [statusAddress, _bump] = await findStatusAddress(distributorAddress, programId);
+    
     const request: ClaimRequest = {
-        bump: bump,
+        bump: _bump,
         index: index,
         amount: amount,
         proof: proof
@@ -170,10 +156,8 @@ export async function claim(
         4000,
     )
 
-    let mintPubkey = new PublicKey('HYAVbi8sM1Nwdv7yjyKoW6fPp29XLCVTkdgdPXnPDCaN');
     const tokenAccountRecipent = Keypair.generate();
     
-    //let claimer = Keypair.generate()
     await getAirdrop(claimer.publicKey)
 
     let tokenAccountRecipentPubkey = await createAccount(
@@ -183,10 +167,7 @@ export async function claim(
       claimer.publicKey, // owner
       tokenAccountRecipent // token account (if you don't pass it, it will use ATA for you)
     );
-    const tokenAccountSender = new PublicKey("4oMD82bVkqrMbCrShDQVqJDMd1GSd5k1rj6a35wKqx6i");
-    const distributorAddress = new PublicKey("3yqZ4JyzjgpKPwESnJRDpjJGht4EFx8HRpmNwXtp9Se9")
-    
-    const [statusAddress, _bump] = await findStatusAddress(distributorAddress, programId);
+  
 
     const keys: AccountMeta[] = [
         <AccountMeta>{pubkey: distributorAddress, isSigner: false, isWritable: true},
@@ -230,26 +211,105 @@ export async function findStatusAddress(
 }
 
 
+export async function createAccountDistributor():Promise<CreateAccountDistributor>{
+    let payer = Keypair.generate();
+    const tokenAccount = Keypair.generate();
+
+    await getAirdrop(payer.publicKey)
+    let mintPubkey = await createMint(
+        connection, // conneciton
+        payer, // fee payer
+        payer.publicKey, // mint authority
+        payer.publicKey, // freeze authority (you can use `null` to disable it. when you disable it, you can't turn it on again)
+        8 // decimals
+      );
+    const tokenAccountEx = await getOrCreateAssociatedTokenAccount(
+    connection,
+    payer,
+    mintPubkey,
+    payer.publicKey
+    )
+    
+    const [distributorAddress, _bump] = await findDistributorAddress(payer.publicKey, programId);
+    let tx = new Transaction().add(
+    // create token account
+    SystemProgram.createAccount({
+        fromPubkey: payer.publicKey,
+        newAccountPubkey: tokenAccount.publicKey,
+        space: ACCOUNT_SIZE,
+        lamports: await getMinimumBalanceForRentExemptAccount(connection),
+        programId: TOKEN_PROGRAM_ID,
+    }),
+    // init mint account
+    createInitializeAccountInstruction(
+        tokenAccount.publicKey, // token account
+        mintPubkey, // mint
+        distributorAddress  //owner of token account
+        //payer.publicKey
+    )
+    );
+
+    console.log(`txhash: ${await connection.sendTransaction(tx, [payer, tokenAccount])}`);
+
+    let createAccountDistributor :CreateAccountDistributor = {
+    mint: mintPubkey,
+    payer: payer,
+    tokenAccountSender: tokenAccount.publicKey,
+    distributorAddress: distributorAddress,
+    bump: _bump
+    };
+   
+    return createAccountDistributor
+}
+
 const kpOne = Keypair.generate();
 const kpTwo = Keypair.generate();
 const kpThree = Keypair.generate();
 const claimer = Keypair.generate();
 
-const leaves = [claimer.publicKey.toString()].map(x => keccak256(x))
-//const leaves  = [claimer.publicKey.toString()]
+const buf = Buffer.concat([
+    new BN(1).toArrayLike(Buffer, "le", 8),
+    claimer.publicKey.toBuffer(),
+    new BN(0).toArrayLike(Buffer, "le", 8),
+  ]);
+
+const leaves = [
+    keccak_256(
+        Buffer.concat([
+            new BN(1).toArrayLike(Buffer, "le", 8),
+            claimer.publicKey.toBuffer(),
+            new BN(0).toArrayLike(Buffer, "le", 8),
+        ])
+    ),
+    keccak_256(
+        Buffer.concat([
+            new BN(1).toArrayLike(Buffer, "le", 8),
+            kpOne.publicKey.toBuffer(),
+            new BN(0).toArrayLike(Buffer, "le", 8),
+        ])
+    )
+]
+
 const tree = new MerkleTree(leaves, keccak256)
-const root = tree.getRoot().toString('hex')
-const leaf = keccak256(claimer.publicKey.toString())
+const root = tree.getRoot()
+const leaf = Buffer.from(keccak_256(buf), "hex")
 const proof = tree.getProof(leaf)
-console.log(root)
-console.log(tree.verify(proof, leaf, root)) // true
 let buffer: Buffer[] = [];
 proof.forEach(x=> buffer.push(x.data))
 
-//createDistributor(254, Buffer.from(root), new BN(10), programId)
-claim(254, new BN(1),new BN(0), buffer)
 
+const main = async () => {
+    const distributorAccount = await createAccountDistributor();
+    createDistributor(Buffer.from(root), new BN(10), programId, distributorAccount.mint, distributorAccount.payer, distributorAccount.distributorAddress, distributorAccount.bump)
+    claim(new BN(1),new BN(0), buffer, distributorAccount.mint, distributorAccount.tokenAccountSender, distributorAccount.distributorAddress)
+}
 
-
+main()
+  .then(() => {
+    console.log("Success");
+  })
+  .catch((e) => {
+    console.error(e);
+  });
 
 
